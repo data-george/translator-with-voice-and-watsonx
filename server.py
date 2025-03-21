2aa81a0c261da1aceee47f7cf0bb5f00850e3604
data-george <georgefrancispinto@gmail.com>
2025-03-21 17:47:33 -0400

    add details to speech to text route on server script

--- server.py
+++ server.py
@@ -16,7 +16,19 @@ def index():
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    print("processing Speech-to-Text")
+    audio_binary = request.data # Get the user's speech from their request
+    text = speech_to_text(audio_binary) # Call speech_to_text function to transcribe the speech
+
+    # Return the response to user in JSON format
+    response = app.response_class(
+        response=json.dumps({'text': text}),
+        status=200,
+        mimetype='application/json'
+    )
+    print(response)
+    print(response.data)
+    return response
 
 
 @app.route('/process-message', methods=['POST'])
